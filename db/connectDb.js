f6367592687ff39dfa058ee1eda2de4dc8277397
Abdulraufcodes <raufshaikh1103@gmail.com>
2026-01-29 19:50:49 +0530

    fixed process.env quotes bug

--- db/connectDb.js
+++ db/connectDb.js
@@ -4,11 +4,12 @@ import mongoose from "mongoose";
 
 const connectDb = async() => {
     try{
-        const conn = await mongoose.connect('process.env.MONGO_URI');
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         // console.log(`mongoDb connected: {conn.conection.host}`);
     }catch(error) {
         // console.log(error.message);
-        process.exit(1);
+        // process.exit(1);
+        console.error("FULL ERROR:", error);
     }
 }
 
